957a21e689f0b26748e4edae5e9250849cb74122
rndomingos <167230659+rndomingos@users.noreply.github.com>
2024-04-24 11:53:07 -0300

    [SO-3190747] fix(ESTG) Modified class

--- Estagiario.java
+++ Estagiario.java
@@ -1,12 +1,14 @@
+
 public class {
+    
     private String nome;
     private double salario;
+    private int cargaHoraria;
 
-    public Estagiario(String nome, double salario){
+    public Estagiario(String nome, double salario, int cargaHoraria){
         this.nome = nome;
         this.salario = salario;
+        this.cargaHoraria = cargaHoraria;
     }
-
-    
     
 }
