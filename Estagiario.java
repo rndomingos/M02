2af350fcedc981635825ddf8f49b9f2429aa02b3
Richard Nathan Domingos <richard.nathan@philips.com>
2024-04-24 11:34:44 -0300

    [SO-3190747] feat(ESTG) New class added

---
+++ Estagiario.java
@@ -0,0 +1,12 @@
+public class {
+    private String nome;
+    private double salario;
+
+    public Estagiario(String nome, double salario){
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    
+    
+}
