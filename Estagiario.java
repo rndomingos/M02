574d6372d342e2a1773662966db8e298229fb0ab
Richard Nathan Domingos <richard.nathan@philips.com>
2024-04-24 11:58:41 -0300

    [SO-3190747] fix(ESTG) Modified class ESTG

--- Estagiario.java
+++ Estagiario.java
@@ -1,12 +1,19 @@
 public class {
     private String nome;
     private double salario;
+    private int porcentagemTreinamento;
 
     public Estagiario(String nome, double salario){
         this.nome = nome;
         this.salario = salario;
     }
 
-    
+    public void aprenderTrabalhando(){
+        this.porcentagemTreinamento += 1;
+    }
+
+    public int getPorcentagemTreinamento(){
+        return this.porcentagemTreinamento;
+    }
     
 }
