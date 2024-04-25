39071b1a90ca82ae44a8582789efec8db860dc76
rndomingos <167230659+rndomingos@users.noreply.github.com>
2024-04-25 09:20:13 -0300

    [SO-3190747] fix(ESTG) Conflict resolved

--- Estagiario.java
+++ Estagiario.java
@@ -4,11 +4,20 @@ public class {
     private String nome;
     private double salario;
     private int cargaHoraria;
+    private int porcentagemTreinamento;
 
     public Estagiario(String nome, double salario, int cargaHoraria){
         this.nome = nome;
         this.salario = salario;
         this.cargaHoraria = cargaHoraria;
+    }
+
+    public void aprenderTrabalhando(){
+        this.porcentagemTreinamento += 1;
+    }
+
+    public int getPorcentagemTreinamento(){
+        return this.porcentagemTreinamento;
     }
     
 }
